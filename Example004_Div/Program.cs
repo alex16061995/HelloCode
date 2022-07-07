b6898469ae5707a4ab204aef0d341b173dfa53de
Aleksei <ustinov@bmgspb.ru>
2022-07-07 15:03:33 +0300

    Add Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,11 @@
+﻿double numberA = 12;
+double numberB = 2;
+
+if (numberB != 0)
+    {
+        Console.WriteLine(numberA / numberB);
+    }
+else 
+    {
+        Console.WriteLine("На 0 делить нельзя!");
+    }
