ca659a2c711bf823a270f7c05ccb1dbdcdd218de
Aleksei <ustinov@bmgspb.ru>
2022-07-07 14:55:27 +0300

    Add Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA;
+int numberB;
+
+Console.Write("Введите первое число ");
+numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Сумма чисел равна " + (numberA + numberB));
+
