bddc71d0a1051e5997be792c5520fc3226e00af9
Aleksei <ustinov@bmgspb.ru>
2022-07-08 21:03:37 +0300

    Add Example009

---
+++ Example009/Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b;
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b || b * b == a)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
